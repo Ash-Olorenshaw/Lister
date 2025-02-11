ae149b7f0a3477bce54232ef9606ae6416732e23
Ash Olorenshaw <asholorenshaw@gmail.com>
2025-02-11 14:04:56 +1100

    Vastly improved logic and function declarations. Fully implemented --coloured and --ignore.
    - Most functions now use currying when it makes sense and only use tuples when it seems appropriate - changes made for speed and readibility of code.
    
    - Added generic function for checking flags
    
    - Added --ignore flag for ignoring dirs, files, etc that start with '.'
    
    - Tidied up helpfile with better info, etc on program operation.

--- Program.fs
+++ Program.fs
@@ -1,45 +1,80 @@
 ﻿// Lister
 // Created by Ash Olorenshaw
-// find it on Github here: 
+// find it on Github here: https://github.com/Ash-Olorenshaw/Lister
 
 module lister
 
 open System.IO
 
-let listFiles(path : string) = 
+let explanation = "[lister.dll] REQUIRED: [<filepath>] OPTIONAL: [-d/--directories] [-f/--files] [-c/--coloured/--colored] [-i/--ignore] [-h/--help]"
+
+let listFiles (path : string) (ignore : bool) = 
     Directory.GetFiles(path)
     |> Array.map Path.GetFileName
+    |> Array.choose (fun file ->
+        if not <| ignore then
+            if not (file[0] = '.') then
+                Some(file)
+            else
+                None
+        else
+            Some(file)
+        )
     |> Array.iter (printfn "%s")
 
+let colourFile (file : string) (dir : bool) = 
+    if dir then
+        $"\u001B[36m{file}\u001B[0m"
+    else
+        file
 
-let listDirs(path : string) = 
+let listDirs(path : string) (colourDir : bool) (ignore : bool) = 
     Directory.GetDirectories(path)
     |> Array.map Path.GetFileName
+    |> Array.choose (fun dir ->
+        if not <| ignore then
+            if not (dir[0] = '.') then
+                Some(dir)
+            else
+                None
+        else
+            Some(dir)
+    )
+    |> Array.map (fun dir -> 
+        if colourDir then
+            colourFile dir true
+        else 
+            dir
+    )
     |> Array.iter (printfn "%s")
 
-let assignIcon(item : string) = 
-    0
-
+let arrayContainsFlags (flag : string, altFlag : string) (targetArray : array<string>) = 
+    if Array.contains flag targetArray || Array.contains altFlag targetArray then
+        true
+    else
+        false
 
 [<EntryPoint>]
 let main(args) = 
-    if Array.contains "-h" args || Array.contains "--help" args then
-        printfn "\n\tLister\n\"Everybody's a textfile, Dave\"\n\n[lister.dll] REQUIRED: [<filepath>] OPTIONAL: [-d/--directories] [-f/--files] [-h/--help]"
+    if arrayContainsFlags ("-h", "--help") args then
+        printfn $"\n\tLister\n\"Everybody's a textfile, Dave\"\n\n{explanation}"
+
     if args.Length > 0 then
-        if Array.contains "-d" args || Array.contains "--directories" args then
-            listDirs args[0]
+        let colouredOut = arrayContainsFlags ("-c", "--coloured") args || Array.contains "--colored" args
+        let ignoreHidden = not <| arrayContainsFlags ("-i", "--ignore") args
 
+        if arrayContainsFlags ("-d", "--directories") args then
+            listDirs args[0] colouredOut ignoreHidden
 
-        if Array.contains "-f" args || Array.contains "--files" args then
-            listFiles args[0]
+        if arrayContainsFlags ("-f", "--files") args then
+            listFiles args[0] ignoreHidden
 
-        else if not <| Array.contains "-d" args && not <| Array.contains "--directories" args then
-            listDirs args[0]
-            listFiles args[0]
-
+        else if not <| arrayContainsFlags ("-d", "--directories") args then
+            listDirs args[0] colouredOut ignoreHidden
+            listFiles args[0] ignoreHidden
 
     else
-        printfn "Error - no filepath specified.\n\nMake sure you're using the right syntax:\n[lister.dll] REQUIRED: [<filepath>] OPTIONAL: [-d/--directories] [-f/--files] [-h/--help]"
+        printfn $"Error - no filepath specified.\n\nMake sure you're using the right syntax:\n{explanation}"
 
     0
 
